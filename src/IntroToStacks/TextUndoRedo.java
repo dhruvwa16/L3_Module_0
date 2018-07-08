d90b695b8291e5354e5fee38b6d63934f8317212
dhruvwa16 <league@wts-imac-11.attlocal.net>
2018-07-07 17:26:07 -0700

    TextUndoRedo

--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -1,6 +1,43 @@
 package IntroToStacks;
 
+import java.awt.Font;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class TextUndoRedo {
+	JFrame frame;
+	JPanel panel;
+	JLabel label;
+	Font font;
+	public static void main(String[] args) {
+		TextUndoRedo hi = new TextUndoRedo();
+		hi.makeTextUndoRedo();
+		
+	}
+
+	private void makeTextUndoRedo() {
+		// TODO Auto-generated method stub
+		frame = new JFrame();
+		panel = new JPanel();
+		label = new JLabel();
+		frame.add(panel);
+		frame.add(label);
+		frame.setVisible(true);
+		font = new Font("Arial",Font.PLAIN,48);
+		
+	}
+	public void keyPressed(KeyEvent b) {
+		// TODO Auto-generated method stub
+		char character = b.getKeyChar();
+		label.setText(character +"");
+		label.setFont(font);
+		System.out.print("hi");
+		
+
+	}
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,5 +50,5 @@ public class TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
-	
+
 }
